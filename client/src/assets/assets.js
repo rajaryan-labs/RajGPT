02b3adc3b5ccaefb04dc3ccfc9c34ffe28d1ff4e
Raj Aryan <rajaryanfront@gmail.com>
2026-03-23 23:21:13 +0530

    refactor: update UI components and dependencies

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -47,10 +47,10 @@ export const assets = {
 
 export const dummyUserData = {
   _id: "689c6deed410acddc0d95a0e",
-  name: "GreatStack",
+  name: "Raj Aryan",
   email: "admin@example.com",
   password: "$2b$10$VESVdPDjL5LF.KCU6jKyqeXNSLASAAfpR2kkIJExtMO.PJvZJAudy",
-  credits: 200,
+  credits: 2000,
 };
 
 // copy for server controllers
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,12 +3,14 @@ import { useAppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 import moment from "moment";
 
-const Sidebar = () => {
+const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
   const { chats, setSelectedChat, theme, setTheme, user, navigate } =
     useAppContext();
   const [search, setSearch] = useState("");
   return (
-    <div className="flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1">
+    <div
+      className={`flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1 ${!isMenuOpen && "max-md:-translate-x-full"}`}
+    >
       {/* logo */}
       <img
         src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
@@ -45,6 +47,11 @@ const Sidebar = () => {
           )
           .map((chat) => (
             <div
+              onClick={() => {
+                navigate("/");
+                setSelectedChat(chat);
+                setIsMenuOpen(false);
+              }}
               key={chat._id}
               className="p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group"
             >
@@ -70,6 +77,7 @@ const Sidebar = () => {
       <div
         onClick={() => {
           navigate("/community");
+          setIsMenuOpen(false);
         }}
         className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all"
       >
@@ -89,15 +97,15 @@ const Sidebar = () => {
         <img src={assets.diamond_icon} className="w-4.5 dark:invert" />
         <div className="flex flex-col text-sm ">
           <p>Credits: {user?.credits}</p>
-          <p className="text-xs text-gray-400 ">
-            Purchase Credits to use QuickGPT
+          <p className="text-xs text-gray-400">
+            Purchase credits to use quickgpt
           </p>
         </div>
       </div>
       {/* Dark Mode Toggle  */}
       <div className="flex items-center justify-between gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md">
         <div className="flex items-center gap-2 text-sm">
-          <img src={assets.theme_icon} className="w-4 not-dark:invert" alt="" />
+          <img src={assets.theme_icon} className="w-4 not-dark:invert" />
           <p>Dark Mode</p>
         </div>
         <label className="relative inline-flex cursor-pointer">
@@ -111,6 +119,27 @@ const Sidebar = () => {
           <span className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4"></span>
         </label>
       </div>
+
+      {/* User Account */}
+      <div className="flex items-center gap-3 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer group">
+        <img src={assets.user_icon} className="w-7 rounded-full" />
+        <div className="flex flex-col text-sm ">
+          <p className="flex-1 text-sm dark:text-primary truncate">
+            {user ? user.name : "Login Your Account"}
+          </p>
+          {user && (
+            <img
+              src={assets.logout_icon}
+              className="h-5 cursor-pointer hidden not-dark:invert group-hover:block "
+            />
+          )}
+        </div>
+      </div>
+      <img
+        src={assets.close_icon}
+        className="absolute top-3 right-3 w-5 h-5 cursor-pointer md:hidden not-dark:invert"
+        onClick={() => setIsMenuOpen(false)}
+      />
     </div>
   );
 };
