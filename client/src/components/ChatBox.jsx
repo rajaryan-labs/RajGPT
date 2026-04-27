e188c5f6b14ef6ca8e448a865e801a01b105a95b
Raj Aryan <rajaryanfront@gmail.com>
2026-04-27 22:35:56 +0530

    Fix: resolve chat deletion and chatbox reply bugs

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useRef, useState } from "react";
 import { useAppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 import Message from "./Message";
+import toast from "react-hot-toast";
 
 const ChatBox = () => {
   const containerRef = useRef(null);
 
-  const { selectedChat, theme } = useAppContext();
+  const { selectedChat, theme, user, setUser, axios, token } = useAppContext();
 
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -15,7 +16,72 @@ const ChatBox = () => {
   const [isPublished, setIsPublished] = useState(false);
 
   const onSubmit = async (e) => {
-    e.preventDefault();
+    try {
+      e.preventDefault();
+
+      if (!user) return toast("Login to send message");
+
+      setLoading(true);
+
+      const promptCopy = prompt;
+      setPrompt("");
+
+      setMessages((prev) => [
+        ...prev,
+        {
+          role: "user",
+          content: prompt,
+          timestamp: Date.now(),
+          isImage: false,
+        },
+      ]);
+
+      const { data } = await axios.post(
+        `/api/message/${mode}`,
+        {
+          chatId: selectedChat._id,
+          prompt,
+          isPublished,
+        },
+        {
+          headers: { Authorization: token },
+        },
+      );
+
+      if (data.success) {
+        const replyMessage =
+          typeof data.message === "string"
+            ? {
+                role: "assistant",
+                content: data.message,
+                timestamp: Date.now(),
+                isImage: false,
+              }
+            : data.message;
+        setMessages((prev) => [...prev, replyMessage]);
+
+        // decrease credits
+        if (mode === "image") {
+          setUser((prev) => ({
+            ...prev,
+            credits: prev.credits - 2,
+          }));
+        } else {
+          setUser((prev) => ({
+            ...prev,
+            credits: prev.credits - 1,
+          }));
+        }
+      } else {
+        toast.error(data.message);
+        setPrompt(promptCopy);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setPrompt("");
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
