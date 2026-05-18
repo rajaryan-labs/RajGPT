a2236f8407677c68a0e2e4e65ef099c5249b2999
Raj Aryan <rajaryanfront@gmail.com>
2026-05-18 12:49:05 +0530

    fix: revert database name to quickgpt to restore user accounts

--- server/configs/db.js
+++ server/configs/db.js
@@ -13,7 +13,7 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () =>
       console.log("Database Connected"),
     );
-    await mongoose.connect(`${process.env.MONGODB_URI}/rajgpt`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/quickgpt`);
   } catch (error) {
     console.log(`${error.message} Error Occured `);
   }
